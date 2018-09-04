2f16c1ac5a8774db696dcd1e6fde1105d457242a
AaronGoddard <aaronbear@gmail.com>
2018-09-04 15:20:44 -0700

    Implement most easy questions on project

---
+++ import_db.sql
@@ -0,0 +1,82 @@
+PRAGMA foreign_keys = ON;
+
+DROP TABLE question_follows;
+DROP TABLE question_likes;
+DROP TABLE questions;
+DROP TABLE replies;
+DROP TABLE users;
+
+
+CREATE TABLE users(
+  id INTEGER PRIMARY KEY,
+  fname VARCHAR(255) NOT NULL,
+  lname VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE questions(
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  body VARCHAR(255),
+  author INTEGER,
+  FOREIGN KEY (author) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows(
+  user_id  INT NOT NULL,
+  question_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+CREATE TABLE replies(
+  id INTEGER PRIMARY KEY,
+  question_id INT,
+  parent_id INT,
+  replier_id INT NOT NULL,
+  subject VARCHAR(255) NOT NULL,
+  body VARCHAR(500),
+  FOREIGN KEY (parent_id) REFERENCES replies(id)
+  --attempt to associate parent reply with child reply
+);
+
+CREATE TABLE question_likes(
+  user_id INT NOT NULL,
+  question_id INT NOT NULL,
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN Key (user_id) REFERENCES users(id)
+);
+
+INSERT INTO 
+users (fname, lname)
+  VALUES
+  ('Artemis', 'Entreri'), ('Bilbo', 'Baggins'), ('Harry', 'Potter'), ('Hiro', 'Protagonist');
+
+INSERT INTO 
+questions (title, body, author)
+VALUES 
+ ('How does this thing work?', "I can't figure this out, can someone please help me? Anyone but You. Know. WHo", 1),
+ ('What have I got in my pocket?', 'Oh bother. It"s dark. I can scarcely tell what is in my waistcoat pocket', 2),
+ ('Hiring Practices', 'Seriously, stop assigning villains to the Defense against the Dark Arts department!', 3),
+ ('Pizza delivery in the FInancial District', 'Where are we supposed to stop here? It seems every blasted spot is a no stopping zone@', 4);
+ 
+ INSERT INTO 
+ replies (question_id, parent_id, replier_id, subject, body)
+ VALUES
+ (1, NULL, 2, 'Darkness', "I wish I could help you, but I can't see more than the pale glow of my mobile device"),
+ (1, 1, 1, "", "");
+ 
+ INSERT INTO
+ question_likes (user_id, question_id)
+ VALUES (4, 3);
+ 
+ INSERT INTO
+ question_follows (user_id, question_id)
+ VALUES (3, 2);
+ 
+ INSERT INTO 
+ users (fname, lname)
+VALUES
+   ('Hiro', 'Yuuy'), 
+   ('Frodo', 'Baggins');
+ 
+ 
---
+++ questions.rb
@@ -0,0 +1,137 @@
+require 'sqlite3'
+require 'singleton'
+
+class QuestionsDatabase < SQLite3::Database
+  include Singleton
+  def initialize
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+  
+class Question
+  attr_accessor :title, :author_id, :body
+  attr_reader :id
+  
+  def self.find_by_id(id)
+    query = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT *
+      FROM questions
+      WHERE id = ?
+    SQL
+    return nil if query.first == nil
+    Question.new(query.first)
+  end
+  
+  def self.find_by_author_id(author_id)
+    query = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+    SELECT * FROM questions WHERE author_id = ?;
+    SQL
+    query.map {|hash| Question.new(hash)}
+  end
+  
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @author = options['author']
+    @body = options['body']
+  end
+  
+  def author
+    query = User.find_by_id(@author)
+    "#{query.fname} #{query.lname}"
+  end
+  
+  def replies
+    replies = Reply.find_by_question(@id)
+    puts "REGARDING: #{@title}"
+    replies.each do |reply|
+      puts ""
+      puts "SUBJECT: #{reply.subject}"
+      puts reply.body
+      puts ""
+    end
+  end
+  
+end
+
+class User
+  attr_accessor :fname, :lname
+  attr_reader :id
+  
+  def self.find_by_id(id)
+    query = QuestionsDatabase.instance.execute(<<-SQL,id)
+      SELECT *
+      FROM users
+      WHERE id = ?
+    SQL
+    return nil if query.first == nil
+    User.new(query.first)
+  end
+  
+  def self.find_by_name(fname, lname)
+    query = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+       SELECT * FROM users WHERE fname = ? AND lname = ?
+    SQL
+  end
+  
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+  
+  def authored_questions
+    Question.find_by_author_id(@id)
+  end
+  
+  def authored_replies
+    Reply.find_by_user_id(@id)
+  end
+
+end
+
+class Reply
+  attr_reader :id
+  attr_accessor :subject, :parent_id, :replier_id, :body
+  def self.find_by_id(id)
+    query = QuestionsDatabase.instance.execute(<<-SQL, id)
+    SELECT * FROM replies WHERE id = ?;
+    SQL
+    return nil if query.first == nil
+    Reply.New(query.first)
+  end
+  
+  def self.find_by_user_id(user_id)
+    query = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+    SELECT * FROM replies WHERE user_id = ?;
+    SQL
+    return nil if query.first == nil
+    query.map {|hash| Reply.new(hash)}
+  end
+  
+  def self.find_by_question(question_id)
+    query = QuestionsDatabase.instance.execute(<<-SQL,question_id)
+    SELECT * FROM replies WHERE question_id = ?;
+    SQL
+    query.map {|el| Reply.new(el)}
+  end
+  
+  def initialize(options)
+    @id = options['id']
+    @subject = options['subject']
+    @body = options['body']
+    @parent_id = options['parent_id']
+    @replier_id = options['replier_id']
+  end
+  
+  def reply
+  end
+end 
+
+
+class Like 
+  attr_reader :user_id, :question_id
+  
+end
